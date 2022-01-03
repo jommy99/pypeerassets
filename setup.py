2daa3facd55ef2523ab35ae8257449d90a4b6b40
John Moore <mr.johnmoore@yahoo.com>
2022-01-03 10:28:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='pypeerassets',
-      version='0.4.8',
+      version='0.4.9',
       description='Python implementation of the PeerAssets protocol.',
       keywords=["blockchain", "digital assets", "protocol"],
-      url='https://github.com/peerassets/pypeerassets',
+      url='https://github.com/jommy99/pypeerassets',
       author='PeerAssets',
       author_email='peerchemist@protonmail.ch',
       license='BSD',
